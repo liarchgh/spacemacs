ae5a47cfea20c9ed6e2cbbcdb009d9ef9c3492c5
Xavier <liarchms@outlook.com>
2025-05-07 12:24:37 +0800

    add python layer

---
+++ elpa/30.1/develop/pydoc-20221222.1426/pydoc-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pydoc" "20221222.1426"
+  "Functional, syntax highlighted pydoc navigation."
+  ()
+  :url "https://github.com/statmobile/pydoc"
+  :commit "c9d8b5249c1b3814da062ed9976e4137d6e8d869"
+  :revdesc "c9d8b5249c1b"
+  :keywords '("pydoc" "python")
+  :authors '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainers '(("Brian J. Lopes" . "statmobile@gmail.com")))
---
+++ elpa/30.1/develop/pytest-20230810.1218/pytest-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pytest" "20230810.1218"
+  "Easy Python test running in Emacs."
+  '((s "1.9.0"))
+  :url "https://github.com/ionrock/pytest-el"
+  :commit "8692f965bf4ddf3d755cf1fbf77a7a768e22460e"
+  :revdesc "8692f965bf4d"
+  :keywords '("pytest" "python" "testing"))
